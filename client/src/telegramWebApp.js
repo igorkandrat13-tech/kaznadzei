135e2ea92c1217cbf98ae9fcb08356c77fc3bd27
igorkandrat13-tech <igorkandrat13@gmail.com>
2026-09-10 20:44:47 +0300

    23454345

--- client/src/telegramWebApp.js
+++ client/src/telegramWebApp.js
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿﻿﻿﻿const TELEGRAM_SESSION_STORAGE_KEY = 'kaznadzei.telegram_webapp';
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿const TELEGRAM_SESSION_STORAGE_KEY = 'kaznadzei.telegram_webapp';
 const TELEGRAM_INIT_DATA_STORAGE_KEY = 'kaznadzei.telegram_init_data';
 const TELEGRAM_UNSAFE_USER_STORAGE_KEY = 'kaznadzei.telegram_unsafe_user';
 const TELEGRAM_EMPLOYEE_SESSION_TOKEN_KEY = 'kaznadzei.telegram_employee_session_token';
